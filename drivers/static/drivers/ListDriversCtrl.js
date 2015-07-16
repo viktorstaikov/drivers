a3b11f544dd8a50fea8bd9b4a265a5716353cddd
Viktor Staykov <viktor.staikov@gmail.com>
2015-07-16 17:35:02 +0200

    added the file upload in the UI

--- drivers/static/drivers/ListDriversCtrl.js
+++ drivers/static/drivers/ListDriversCtrl.js
@@ -1,42 +1,53 @@
-angular.module('ListDriversCtrl', []).controller('ListDriversController', ['$scope', '$http', 'DriversService', function ($scope, $http, driversService) {
-    $scope.errorMsg = '';
+angular
+    .module('ListDriversCtrl', ['angularFileUpload'])
+    .controller('ListDriversController', ['$scope', '$route', '$http', 'DriversService', 'FileUploader', function ($scope, $route, $http, driversService, FileUploader) {
+        $scope.errorMsg = '';
 
-    $scope.statusToText = function (driver) {
-        switch (driver.status) {
-        case 1:
-            return 'clean';
-            break;
-        case 2:
-            return 'discrepancy in docs';
-            break;
-        case 3:
-            return 'fake documents';
-            break;
-        default:
-            return 'not checked';
-            break;
+        $scope.uploader = new FileUploader();
+        $scope.uploader.url = '/api/import/driver';
+        $scope.uploader.queueLimit = 1;
+        $scope.uploader.onSuccessItem = function () {
+            $route.reload();
         }
-    }
 
-    $scope.remove = function (driver, index) {
-        $scope.drivers.splice(index, 1);
-        driversService.delete(driver.id);
-    }
+        $scope.statusToText = function (driver) {
+            switch (driver.status) {
+            case 1:
+                return 'clean';
+                break;
+            case 2:
+                return 'discrepancy in docs';
+                break;
+            case 3:
+                return 'fake documents';
+                break;
+            default:
+                return 'not checked';
+                break;
+            }
+        }
 
-    $scope.edit = function (driver) {
-        console.log(driver);
-    }
+        $scope.remove = function (driver, index) {
+            $scope.drivers.splice(index, 1);
+            driversService.delete(driver.id);
+        }
 
-    $scope.export = function () {
-        window.open('/api/export/driver', '_blank', '');
-    }
+        $scope.edit = function (driver) {
+            console.log(driver);
+        }
 
-    $scope.import = function () {
+        $scope.export = function () {
+            window.open('/api/export/driver', '_blank', '');
+        }
 
-    }
+        $scope.import = function () {
+            if ($scope.uploader.queue && $scope.uploader.queue.length > 0) {
+                $scope.uploader.uploadAll();
+            }
+        }
 
-    driversService.getAll()
-        .success(function (response) {
-            $scope.drivers = response.objects;
-        });
-}]);
+        driversService.getAll()
+            .success(function (response) {
+                $scope.drivers = response.objects;
+            });
+    }]);
