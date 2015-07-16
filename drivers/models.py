418443e1fe289c361d3d326b7f63b466b571054a
Viktor Staykov <viktor.staikov@gmail.com>
2015-07-16 15:48:32 +0200

    update driver model: added telnumber,address,car and lastmodified

--- drivers/models.py
+++ drivers/models.py
@@ -1,9 +1,14 @@
 # models
 from peewee import *
+from datetime import datetime
 
 
 class Driver(Model):
     name = CharField(unique=True)
     email = CharField(unique=True)
+    telnumber = CharField(default='')
+    address = CharField(default='')
+    car = CharField(default='')
     status = IntegerField(
         choices=((0, 'not checked'), (1, 'clean'), (2, 'discrepancy in docs'), (3, 'fake documents')), default=0)
+    lastmodified = DateTimeField(default=datetime.now, null=False)
