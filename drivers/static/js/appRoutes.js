ee811b84446c361bf40f4d86d5a3b2635e3d7bfa
Viktor Staykov <viktor.staikov@gmail.com>
2015-07-18 14:20:08 +0200

    static/js/appRoutes.js

--- drivers/static/js/appRoutes.js
+++ drivers/static/js/appRoutes.js
@@ -1,29 +1,24 @@
 angular.module('appRoutes', []).config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
-		$routeProvider
-			.when('/home', {
-				templateUrl: '../static/home/home.html',
-				controller: 'MainController'
-			})
-			.when('/new-driver', {
-				templateUrl: '../static/drivers/new-driver.html',
-				controller: 'NewDriverController'
-			})
-			.when('/list-all-drivers', {
-				templateUrl: '../static/drivers/list-all-drivers.html',
-				controller: 'ListDriversController'
-			})
-			.otherwise({
-				redirectTo: '/home'
-			});
-
-		//$locationProvider.html5Mode(true); // fix url to not contain '#'
-	}])
-	.run(function ($rootScope, $location) {
-		$rootScope.$on('$routeChangeError', function (current, previous, rejection, message) {
-
-			if (message === 'Not Authenticated') {
-				$location.path('/login');
-				$location.search('redirect_url', previous.$$route.originalPath);
-			}
+	$routeProvider
+		.when('/home', {
+			templateUrl: '../static/home/home.html',
+			controller: 'MainController'
 		})
-	})
+		.when('/new-driver', {
+			templateUrl: '../static/drivers/new-driver.html',
+			controller: 'NewDriverController'
+		})
+		.when('/list-all-drivers', {
+			templateUrl: '../static/drivers/list-all-drivers.html',
+			controller: 'ListDriversController'
+		})
+		.when('/signup', {
+			templateUrl: '../static/drivers/new-driver.html',
+			controller: 'NewDriverController'
+		})
+		.otherwise({
+			redirectTo: '/home'
+		});
+
+	//$locationProvider.html5Mode(true); // fix url to not contain '#'
+}])
