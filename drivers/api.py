df69318bedc30a4216fc14704b281b77a69cdea2
Viktor Staykov <viktor.staikov@gmail.com>
2015-07-18 14:19:09 +0200

    add endpoint to get current user info; fix some auth problems

--- drivers/api.py
+++ drivers/api.py
@@ -1,10 +1,15 @@
 from flask_peewee.rest import RestAPI, Authentication, UserAuthentication, RestResource
 from flask_peewee.auth import Auth
 from flask import make_response, request
+from peewee import IntegrityError
 from models import Driver
 from drivers import app, db
 from datetime import datetime
 import json
+
+
+import pdb
+
 
 ALLOWED_EXTENSIONS = ['csv']
 
@@ -39,16 +44,32 @@ api.setup()
 @app.route('/api/signup/', methods=['POST'])
 def register_driver():
     driver = request.get_json()
-    print(json.dumps(driver))
-    Driver.create(username=driver['username'], password=driver[
-        'password'], name=driver['name'], email=driver['email'], telnumber=driver['telnumber'], address=driver['address'], car=driver['car'])
-    return make_response(json.dumps({'result': True}))
+    try:
+        Driver.create(username=driver['username'], password=driver['password'], name=driver['name'], email=driver[
+                      'email'], telnumber=driver['telnumber'], address=driver['address'], car=driver['car'])
+
+        return make_response(json.dumps({'result': True}))
+    except IntegrityError as e:
+        field = e.message.spli('.')[1]
+        msg = 'There is already driver with such ' + field + '.'
+        return make_response(json.dumps({'error': msg})), 400
 
 
-@app.route('/api/export/driver', methods=['GET'])
+@app.route('/api/me/', methods=['GET'])
+@auth.login_required
+def get_me():
+    current_user = auth.get_context_user()['user']
+    me = current_user.__dict__['_data']
+    me['lastmodified'] = me['lastmodified'].isoformat()
+    me.pop('password', None)
+    # pdb.set_trace()
+    return make_response(json.dumps(me))
+
+
+@app.route('/api/export/driver/', methods=['GET'])
 @auth.login_required
 def export_drivers():
-    current_user = self.auth.get_logged_in_user()
+    current_user = auth.get_context_user()['user']
     if not current_user.is_admin:
         return 401
     drivers = Driver.select(
@@ -69,10 +90,10 @@ def allowed_extension(filename):
     return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
 
-@app.route('/api/import/driver', methods=['GET', 'POST'])
+@app.route('/api/import/driver/', methods=['GET', 'POST'])
 @auth.login_required
 def import_background_checks():
-    current_user = self.auth.get_logged_in_user()
+    current_user = auth.get_context_user()['user']
     if not current_user.is_admin:
         return 401
     f = request.files['file']
